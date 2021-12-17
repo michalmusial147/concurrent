3ab2e9958eb9396de2f930e88c9738478ae5094d
mmusial <mmusial@consdata.com>
2021-12-17 22:17:46 +0100

    generated components

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerViewComponent } from './components/server-view/server-view.component';
+import { ClientsViewComponent } from './components/clients-view/clients-view.component';
+import { ClientComponent } from './components/client/client.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerViewComponent,
+    ClientsViewComponent,
+    ClientComponent
   ],
   imports: [
     BrowserModule,
